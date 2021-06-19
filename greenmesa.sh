f24bf40df75d9e336430f65655b8d016c506040d
Rooskie <polyversialmind@gmail.com>
2021-06-18 21:12:27 -0500

    remove dist files to be properly replaced

--- greenmesa.sh
+++ greenmesa.sh
@@ -3,6 +3,8 @@
 cd /var/www/stratum/greenmesa
 pm2 stop stratum \
 && git pull \
+&& echo "Remove extraneous files" \
+&& find ./dist ! -name '.gitignore' -type f -exec rm -f {} + \
 && echo "Building App" \
 && npx tsc \
 && echo "Deploying to PM2" \
